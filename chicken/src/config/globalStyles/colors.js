8011dfb4edc2383481e7940fb473c998b16667da
Max Magee <maxmagee@users.noreply.github.com>
2018-08-05 19:43:04 -0500

    #3 Created tab bar navigation between 5 placeholder pages.

---
+++ chicken/src/config/globalStyles/colors.js
@@ -0,0 +1,7 @@
+const colors = {
+  darkGray: '#756F64',
+  red: '#C30C30',
+  white: '#FFFFFF'
+};
+
+export default colors;
--- chicken/src/index.js
+++ chicken/src/index.js
@@ -1,27 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import RootNavigator from './config/routes/RootNavigator';
 
 // Need the build call so the rest of the EStyleSheet references all work
 EStyleSheet.build({
   // $outline: 1
 });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+export default RootNavigator;
