85afac9a7ea921e266f5336dad7065f9f0e46a8f
Max Magee <maxmagee@users.noreply.github.com>
2018-08-05 23:10:27 -0500

    #5 Added some global styles.

--- chicken/src/config/globalStyles/styles.js
+++ chicken/src/config/globalStyles/styles.js
@@ -2,6 +2,22 @@ const globalStyles = {
   bottomTabBarLabel: {
     fontFamily: 'Arial',
     fontSize: 11
+  },
+  buttonLabel: {
+    fontFamily: 'Arial',
+    fontSize: 16,
+    fontWeight: 11
+  },
+  calloutEmphasized: {
+    fontFamily: 'Times New Roman',
+    fontSize: 19,
+    fontWeight: 'bold'
+  },
+  calloutBig: {
+    fontFamily: 'Arial',
+    fontSize: 38,
+    fontWeight: 'bold',
+    lineHeight: 48
   }
 };
 
