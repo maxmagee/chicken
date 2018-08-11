e2fdc84d29e26e52badab1a14264c23aba196532
Max Magee <maxmagee@users.noreply.github.com>
2018-08-11 16:07:47 -0500

    #19 Legal and License tabs added and styled on TermsAndConditions Screen

--- chicken/src/screens/accountTab/TermsAndConditionsScreen.js
+++ chicken/src/screens/accountTab/TermsAndConditionsScreen.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
-import { StatusBar, Text, View } from 'react-native';
+import { Dimensions, StatusBar } from 'react-native';
 import PropTypes from 'prop-types';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
+import TabBar from 'react-native-underline-tabbar';
+
+import { colors, globalStyles } from '../../config/globalStyles';
+
+import LegalScreen from './LegalScreen';
+import LicenseScreen from './LicenseScreen';
+
+const LEGAL_TAB_WIDTH = 34;
+const LICENSES_TAB_WIDTH = 56;
+const ALL_TABS_WIDTH = LEGAL_TAB_WIDTH + LICENSES_TAB_WIDTH;
+const TOTAL_NUMBER_OF_TABS = 2;
+const TOTAL_WIDTH = Dimensions.get('window').width;
+const TAB_GAP_WIDTH = (TOTAL_WIDTH - ALL_TABS_WIDTH) / (TOTAL_NUMBER_OF_TABS + 1);
 
 class TermsAndConditionsScreen extends Component {
   componentWillUnmount() {
@@ -9,11 +24,27 @@ class TermsAndConditionsScreen extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontFamily: 'Arial', fontSize: 20, color: 'black' }}>
-          Terms & Conditions
-        </Text>
-      </View>
+      <ScrollableTabView
+        renderTabBar={() => (
+          <TabBar
+            underlineColor={colors.modalNavigationHeaderTabUnderline}
+            tabBarStyle={styles.tabBarStyle}
+            tabBarTextStyle={[
+              globalStyles.modalNavigationHeaderTabLabel,
+              { color: colors.modalNavigationHeaderTabInactive }
+            ]}
+            activeTabTextStyle={[
+              globalStyles.modalNavigationHeaderTabLabel,
+              { color: colors.modalNavigationHeaderTabActive }
+            ]}
+            underlineHeight={4}
+            tabMargin={TAB_GAP_WIDTH}
+          />
+        )}
+      >
+        <LegalScreen tabLabel={{ label: 'Legal' }} />
+        <LicenseScreen tabLabel={{ label: 'Licenses' }} />
+      </ScrollableTabView>
     );
   }
 }
@@ -21,5 +52,14 @@ class TermsAndConditionsScreen extends Component {
 TermsAndConditionsScreen.propTypes = {
   navigation: PropTypes.object
 };
+
+const styles = EStyleSheet.create({
+  tabBarStyle: {
+    backgroundColor: colors.modalNavigationHeaderBackground,
+    marginTop: 0,
+    paddingTop: 20
+  },
+  tabBarText: {}
+});
 
 export default TermsAndConditionsScreen;
