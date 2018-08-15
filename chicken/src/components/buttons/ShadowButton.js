4b9b1a6365a1d89f34cb44905190978b126736d6
Max Magee <maxmagee@users.noreply.github.com>
2018-08-15 18:04:06 -0500

    #23 Adjusted styling of ShadowButton.

--- chicken/src/components/buttons/ShadowButton.js
+++ chicken/src/components/buttons/ShadowButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableHighlight, Text } from 'react-native';
+import { TouchableOpacity, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import { LinearGradient } from 'expo';
 import EStyleSheet from 'react-native-extended-stylesheet';
@@ -7,11 +7,11 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 import { colors, globalStyles } from '../../config/globalStyles';
 
 const ShadowButton = ({ label, onPress }) => (
-  <TouchableHighlight onPress={onPress} underlayColor="transparent">
+  <TouchableOpacity onPress={onPress} underlayColor="transparent">
     <LinearGradient colors={[colors.white, colors.white]} style={styles.gradientStyle}>
       <Text style={[globalStyles.buttonLabel, styles.label]}>{label}</Text>
     </LinearGradient>
-  </TouchableHighlight>
+  </TouchableOpacity>
 );
 
 ShadowButton.propTypes = {
@@ -27,7 +27,7 @@ const styles = EStyleSheet.create({
     height: 50,
     shadowColor: colors.black,
     shadowOpacity: 0.3,
-    shadowRadius: 5,
+    shadowRadius: 3,
     shadowOffset: {
       height: 3,
       width: 0
