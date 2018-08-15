8ba994eb374943d9b31be6d2a1926cde401940d6
Max Magee <maxmagee@users.noreply.github.com>
2018-08-15 15:56:54 -0500

    #23 The keyboard will now dismiss if you tap on any portion of the screen.

--- chicken/src/screens/authentication/EmailSignInScreen.js
+++ chicken/src/screens/authentication/EmailSignInScreen.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { Alert, Image, Text, TouchableOpacity, View } from 'react-native';
+import {
+  Alert,
+  Image,
+  Keyboard,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View
+} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 import { colors, globalStyles } from '../../config/globalStyles';
@@ -58,61 +66,62 @@ class EmailSignInScreen extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.containerTop}>
-          <View style={styles.imageContainer}>
-            <Image source={sun} height={160} width={160} />
-          </View>
-          <EllipsisCallout
-            containerStyle={styles.ellipsisContainer}
-            backgroundColor={colors.lightBlue}
-            largeText="Let's get started"
-            emphasizedText="SIGN IN TO CHICK-FIL-A ONE"
-          />
-        </View>
-        <View style={styles.containerBottom}>
-          <FloatingLabelTextInput
-            label="EMAIL ADDRESS"
-            value={this.state.emailAddress}
-            onChangeText={this.handleEmailChangeText}
-            autoCapitalize="none"
-            autoCorrect={false}
-            autoFocus
-            clearButtonMode="while-editing"
-            keyboardType="email-address"
-            returnKeyType="next"
-          />
-          <FloatingLabelTextInput
-            label="PASSWORD"
-            value={this.state.password}
-            onChangeText={this.handlePasswordChangeText}
-            autoCapitalize="none"
-            autoCorrect={false}
-            clearButtonMode="while-editing"
-            returnKeyType="next"
-            secureTextEntry={this.state.hidePassword}
-          />
-          <View style={styles.passwordButtonsContainer}>
-            <View>
-              <TouchableOpacity onPress={this.handleForgotPassword}>
-                <Text style={globalStyles.smallTextButton}>Forgot password?</Text>
-              </TouchableOpacity>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={styles.container}>
+          <View style={styles.containerTop}>
+            <View style={styles.imageContainer}>
+              <Image source={sun} height={160} width={160} />
             </View>
-            <View>
-              <TouchableOpacity onPress={this.handleTogglePassword}>
-                <Text style={globalStyles.smallTextButton}>{this.state.togglePasswordText}</Text>
-              </TouchableOpacity>
-            </View>
+            <EllipsisCallout
+              containerStyle={styles.ellipsisContainer}
+              backgroundColor={colors.lightBlue}
+              largeText="Let's get started"
+              emphasizedText="SIGN IN TO CHICK-FIL-A ONE"
+            />
           </View>
+          <View style={styles.containerBottom}>
+            <FloatingLabelTextInput
+              label="EMAIL ADDRESS"
+              value={this.state.emailAddress}
+              onChangeText={this.handleEmailChangeText}
+              autoCapitalize="none"
+              autoCorrect={false}
+              clearButtonMode="while-editing"
+              keyboardType="email-address"
+              returnKeyType="next"
+            />
+            <FloatingLabelTextInput
+              label="PASSWORD"
+              value={this.state.password}
+              onChangeText={this.handlePasswordChangeText}
+              autoCapitalize="none"
+              autoCorrect={false}
+              clearButtonMode="while-editing"
+              returnKeyType="next"
+              secureTextEntry={this.state.hidePassword}
+            />
+            <View style={styles.passwordButtonsContainer}>
+              <View>
+                <TouchableOpacity onPress={this.handleForgotPassword}>
+                  <Text style={globalStyles.smallTextButton}>Forgot password?</Text>
+                </TouchableOpacity>
+              </View>
+              <View>
+                <TouchableOpacity onPress={this.handleTogglePassword}>
+                  <Text style={globalStyles.smallTextButton}>{this.state.togglePasswordText}</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
 
-          <View style={{ justifyContent: 'flex-end' }}>
-            <View style={styles.buttonContainer}>
-              <GradientButton label="Sign in" onPress={this.handleSignIn} />
+            <View style={{ justifyContent: 'flex-end' }}>
+              <View style={styles.buttonContainer}>
+                <GradientButton label="Sign in" onPress={this.handleSignIn} />
+              </View>
+              <TermsAgreementWaiver navigation={this.props.navigation} />
             </View>
-            <TermsAgreementWaiver navigation={this.props.navigation} />
           </View>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
