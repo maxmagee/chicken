15a0263670a241fb39c3bcd10438214c0f5d7977
Max Magee <maxmagee@users.noreply.github.com>
2018-09-03 21:57:25 -0500

    #63 The Nearest Locations screen now renders a map and places a pin at your current location.

--- chicken/src/screens/menuTab/NearestLocationsScreen.js
+++ chicken/src/screens/menuTab/NearestLocationsScreen.js
@@ -1,7 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Location, MapView, Permissions } from 'expo';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
-import ComingSoonScreen from '../shared/ComingSoonScreen';
+class NearestLocationsScreen extends Component {
+  constructor(props) {
+    super(props);
 
-const NearestLocationsScreen = () => <ComingSoonScreen />;
+    this.state = {
+      currentLocation: {
+        latitude: 37.78825,
+        longitude: -122.4324
+      },
+      region: this.getInitialRegion()
+    };
+  }
+
+  componentDidMount() {
+    this.getLocationAsync();
+  }
+
+  getInitialRegion = () => {
+    // Should be in San Francisco
+    return {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421
+    };
+  };
+
+  getLocationAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      console.log('Location access permission denied.');
+    }
+
+    const location = await Location.getCurrentPositionAsync({ enableHighAccuracy: false });
+
+    this.setState({
+      currentLocation: {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      },
+      region: {
+        ...this.state.region,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      }
+    });
+  };
+
+  handleRegionChange = () => {};
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={this.getInitialRegion()}
+          region={this.state.region}
+          onRegionChange={this.handleRegionChange}
+        >
+          <MapView.Marker coordinate={this.state.currentLocation} title="Current location" />
+        </MapView>
+      </View>
+    );
+  }
+}
+
+const styles = EStyleSheet.create({
+  container: {
+    flex: 1
+  },
+  map: {
+    flex: 1
+  }
+});
 
 export default NearestLocationsScreen;
